e76f3d6d637e4846d499e94ab5706b8d0f13ff76
benjaminbylund <31540367+benjaminbylund@users.noreply.github.com>
2019-01-11 13:04:34 +0100

    Add files via upload
    Är klar med första sprinten

--- kryptering/src/CMDinput.java
+++ kryptering/src/CMDinput.java
@@ -1,6 +1,34 @@
+
+
 public class CMDinput {
     public static void main(String[] args) {
-        for (int i = 0; 1 < args.length; i++)
-            System.out.println(args[i]);
+
+        if(args.length <= 0) {
+            System.out.println("Programmet vet inte vad det ska göra! Avbryter...");
+        }
+        if(args.length == 1) {
+            System.out.println("så du säger");
+        }
+        if(args.length == 2) { // behandla det som siffor och räkna ut summan
+            int n = 0;
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].matches("[0-9]+")) {
+                    n += Integer.parseInt(args[i]);
+                }else{
+                        System.out.println("Du använder ingen siffra");
+                }
+                System.out.println(n);
+            }
+        }
+        if(args.length == 3) { // Skriv ut argumentet i omvänd ordning
+            for (int i = args.length -1; i >= 0; i--){
+                System.out.println(args[i]);
+            }
+        }
+        if(args.length >= 4) {
+            System.out.println("Jag orkar inte, jag stänger ner");
+
+        }
     }
 }
+
